48fa0a7aa3678364147855eb0513f51f49271139
anniechakraborty <46227364+anniechakraborty@users.noreply.github.com>
2020-05-14 19:35:50 +0530

    Created the window structure for the game

---
+++ pong.py
@@ -0,0 +1,9 @@
+import turtle
+
+# Creating window
+win = turtle.Screen()
+win.title('Pong by @annieC')
+win.bgcolor("black")
+win.setup(width=800, height=600)
+win.tracer(0)
+# this prevents the window from auto updating and makes the game faster - we can update the window manually now.
