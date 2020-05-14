92801799d2a834fc5a31938adda081f454c8bffc
anniechakraborty <46227364+anniechakraborty@users.noreply.github.com>
2020-05-14 19:44:27 +0530

    Added the paddle movements initiated by keyboard inputs.

--- pong.py
+++ pong.py
@@ -37,3 +37,59 @@ pongBall.color("white")
 pongBall.penup()
 pongBall.dx = 0.032
 pongBall.dy = 0.032
+
+
+# Functions to move paddles
+
+def paddleAUp() :
+    y = paddle_a.ycor()
+    # ycor() is a turtle method which returns the y coordinate of a turtle object
+    y += 20
+    # Adding 20 px to the y coordinate
+    paddle_a.sety(y)
+    # setting 20 px to the y coordinate of the paddle a
+
+
+def paddleADown() :
+    y = paddle_a.ycor()
+    # ycor() is a turtle method which returns the y coordinate of a turtle object
+    y -= 20
+    # Subtracting 20 px from the y coordinate
+    paddle_a.sety(y)
+    # setting 20 px to the y coordinate of the paddle a
+
+
+def paddleBUp() :
+    y = paddle_b.ycor()
+    # ycor() is a turtle method which returns the y coordinate of a turtle object
+    y += 20
+    # Adding 20 px to the y coordinate
+    paddle_b.sety(y)
+    # setting 20 px to the y coordinate of the paddle b
+
+
+def paddleBDown() :
+    y = paddle_b.ycor()
+    # ycor() is a turtle method which returns the y coordinate of a turtle object
+    y -= 20
+    # Subtracting 20 px from the y coordinate
+    paddle_b.sety(y)
+    # setting 20 px to the y coordinate of the paddle b
+
+
+# Keyboard binding
+win.listen()  # listens for keyboard inputs
+
+win.onkeypress(paddleAUp, "w")
+# after the listen() makes the window wait for the keyboard input, the above onkeypress() calls the paddleBUp()
+# when the small w key is pressed
+win.onkeypress(paddleADown, "s")
+# after the listen() makes the window wait for the keyboard input, the above onkeypress() calls the paddleBDown()
+# when the small s key is pressed
+
+win.onkeypress(paddleBUp, "Up")
+# after the listen() makes the window wait for the keyboard input, the above onkeypress() calls the paddleBUp()
+# when the Up arrow key is pressed
+win.onkeypress(paddleBDown, "Down")
+# after the listen() makes the window wait for the keyboard input, the above onkeypress() calls the paddleBDown()
+# when the Down arrow key is pressed
